a4899f2c4028001d4dfc93498faf281b1911fd4c
Sam <a>
2021-06-07 16:47:45 +0100

    Updated docs

--- lib/chat_backend.dart
+++ lib/chat_backend.dart
@@ -14,29 +14,27 @@ class Message {
 
 //https://flutter.dev/docs/development/data-and-backend/state-mgmt/simple#changenotifier
 class ChatBackend extends ChangeNotifier {
-  /// Internal, private state of the cart.
+  /// Internal, private state of the messages.
   // TODO Create better messages
   List<Message> _messages = [
     Message("Hello", false),
     Message("How are you?", true),
   ];
 
-  /// An unmodifiable view of the items in the cart.
+  /// An unmodifiable view of the messages for consumers
   UnmodifiableListView<Message> get messages => UnmodifiableListView(_messages);
 
-  /// Adds [message] to cart. This and [removeAll] are the only ways to modify the
-  /// cart from the outside.
+  /// Adds a [Message] to the data store.
+  /// And updates all listeners
   void add(Message message) {
     _messages.add(message);
-    _messages.add(Message("Some cool response", true));
+    _messages.add(calculateCleoResponse(message));
     // This call tells the widgets that are listening to this model to rebuild.
     notifyListeners();
   }
 
-  /// Removes all items from the cart.
-  void removeAll() {
-    _messages.clear();
-    // This call tells the widgets that are listening to this model to rebuild.
-    notifyListeners();
+  /// Calculates what cleo should respond to on a given message
+  Message calculateCleoResponse(Message message) {
+    return Message("Some cool response", true);
   }
 }
